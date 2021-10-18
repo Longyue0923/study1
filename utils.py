4cad3c5cf2b1a76dfa284f0f90473594827492c8
LongYue <1660790354@qq.com>
2021-10-18 21:01:50 +0800

    修改注释

--- utils.py
+++ utils.py
@@ -92,6 +92,7 @@ def sortClusters(cluster):
     return np.concatenate([cluster, ratio[:, None]], axis=1)
 
 
+# 加载数据，返回的数据为gt的宽高
 def load_dataset(path, normalized=True):
     """
         加载 xml 文件
